50861cb819e7165b3fbaf576e81bbc7768be0612
ChaitanyaKadu03 <chaitanyakadu03@gmail.com>
2025-03-21 16:00:30 +0530

    V8 PRNG 0.2v
    The random number generator is implemented.

--- v8_random_predictor/src/main.rs
+++ v8_random_predictor/src/main.rs
@@ -1,5 +1,3 @@
-use std::io::{stdin};
-
 #[derive(Debug)]
 struct Xorshift128 {
     state1: u64,
@@ -11,26 +9,35 @@ impl Xorshift128 {
         Xorshift128 { state1, state2 }
     }
 
-    fn next(&mut self) -> Self {
+    fn next(&mut self) -> f64 {
         let mut s1: u64 = self.state1;
         let s2: u64 = self.state2;
+
+        self.state1 = s2;
 
         s1 ^= s1 << 23;
         s1 ^= s1 >> 17;
         s1 ^= s2;
         s1 ^= s2 >> 26;
 
-        Xorshift128::new(s2, s1)
+        self.state2 = s1;
+
+        let value: u64 = self.state1;
+        let mut value: f64 = (value >> 11) as f64; // float value
+        value = value / (1u64 << 53) as f64; // normalization
+        1f64 - value
+    }
+
+    fn get_state(calculations: Vec<f64>) -> Option<Xorshift128> {
+        // for value in calculations {
+        //     let mut _value: f64 = value + 1f64;
+        //     _value = value * (1u64 )
+        // }
+
+        None
     }
 }
 
 fn main() {
-    let mut sample: Xorshift128 = Xorshift128::new(12345,67890);
-
-    for _ in 0..10 {
-        let temp: Xorshift128 = Xorshift128::next(&mut sample);
-        sample = temp;
-
-        println!("{sample:#?}")
-    }
+    let mut state: Xorshift128 = Xorshift128::new(1233744567890, 3646874774744);
 }
